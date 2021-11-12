506da25e190fb494488684542b587ea5a2f39dc1
Stephen Knupfer <stephenrknupfer@hotmail.com>
2021-11-12 13:18:41 -0300

    Calendar header layout changes

--- src/components/CalendarHeader.tsx
+++ src/components/CalendarHeader.tsx
@@ -7,23 +7,23 @@ import { ICalendarEvent } from '../interfaces'
 import { useTheme } from '../theme/ThemeContext'
 import { isToday, typedMemo } from '../utils'
 
-export interface CalendarHeaderProps {
+export interface CalendarHeaderProps<T> {
   dateRange: dayjs.Dayjs[]
   cellHeight: number
   headerCellHeight?: number
   style: ViewStyle
-  allDayEvents: ICalendarEvent[]
+  allDayEvents: ICalendarEvent<T>[]
   onPressDateHeader?: (date: Date) => void
 }
 
-function _CalendarHeader({
+function _CalendarHeader<T>({
   dateRange,
   cellHeight,
   style,
   allDayEvents,
   onPressDateHeader,
   headerCellHeight,
-}: CalendarHeaderProps) {
+}: CalendarHeaderProps<T>) {
   const _onPress = React.useCallback(
     (date: Date) => {
       onPressDateHeader && onPressDateHeader(date)
@@ -54,7 +54,7 @@ function _CalendarHeader({
       ]}
     >
       <View style={[u['z-10'], u['w-50'], borderColor]} />
-      {dateRange.map((date) => {
+      {dateRange.map((date, index) => {
         const _isToday = isToday(date)
         return (
           <TouchableOpacity
@@ -63,58 +63,76 @@ function _CalendarHeader({
             disabled={onPressDateHeader === undefined}
             key={date.toString()}
           >
-            <View style={[u['justify-between'], { height: headerCellHeight ?? cellHeight }]}>
-              <Text
-                style={[
-                  theme.typography.xs,
-                  u['text-center'],
-                  { color: _isToday ? theme.palette.primary.main : theme.palette.gray['500'] },
-                  {
-                    ...(_isToday
-                      ? theme.customStyles?.dateHeaderTodayDayText
-                      : theme.customStyles?.dateHeaderDayText),
-                  },
-                ]}
-              >
-                {date.format('ddd')}
-              </Text>
+            <View style={{ alignItems: 'stretch' }}>
               <View
-                style={
-                  _isToday
-                    ? [
-                        primaryBg,
-                        u['h-36'],
-                        u['w-36'],
-                        u['pb-6'],
-                        u['rounded-full'],
-                        u['items-center'],
-                        u['justify-center'],
-                        u['self-center'],
-                        u['z-20'],
-                        theme.customStyles?.dateHeaderTodayContainer,
-                      ]
-                    : [u['mb-6']]
-                }
+                style={[
+                  u['border-t'],
+                  theme.isRTL && index > 0 ? u['border-l'] : u['border-r'],
+                  !theme.isRTL && index > 0 ? u['border-r'] : u['border-l'],
+                  borderColor,
+                  { paddingVertical: 8 },
+                ]}
               >
                 <Text
                   style={[
-                    {
-                      color: _isToday
-                        ? theme.palette.primary.contrastText
-                        : theme.palette.gray['800'],
-                    },
-                    theme.typography.xl,
+                    theme.typography.xs,
                     u['text-center'],
-                    Platform.OS === 'web' && _isToday && u['mt-6'],
+                    { color: _isToday ? theme.palette.primary.main : theme.palette.gray['500'] },
                     {
                       ...(_isToday
-                        ? theme.customStyles?.dateHeaderTodayText
-                        : theme.customStyles?.dateHeaderText),
+                        ? theme.customStyles?.dateHeaderTodayDayText
+                        : theme.customStyles?.dateHeaderDayText),
                     },
                   ]}
                 >
-                  {date.format('D')}
+                  {date.format('ddd')}
                 </Text>
+              </View>
+              <View
+                style={[
+                  u['border-t'],
+                  theme.isRTL && index > 0 ? u['border-l'] : u['border-r'],
+                  !theme.isRTL && index > 0 ? u['border-r'] : u['border-l'],
+                  borderColor,
+                  { paddingVertical: 8 },
+                ]}
+              >
+                <View
+                  style={
+                    _isToday
+                      ? [
+                          primaryBg,
+                          u['rounded-full'],
+                          u['items-center'],
+                          u['justify-center'],
+                          u['self-center'],
+                          u['z-20'],
+                          { width: 30, height: 30, borderRadius: 15 },
+                          borderColor,
+                          theme.customStyles?.dateHeaderTodayContainer,
+                        ]
+                      : {}
+                  }
+                >
+                  <Text
+                    style={[
+                      {
+                        color: _isToday
+                          ? theme.palette.primary.contrastText
+                          : theme.palette.gray['800'],
+                      },
+                      _isToday ? { fontSize: 16 } : theme.typography.xl,
+                      u['text-center'],
+                      {
+                        ...(_isToday
+                          ? theme.customStyles?.dateHeaderTodayText
+                          : theme.customStyles?.dateHeaderText),
+                      },
+                    ]}
+                  >
+                    {date.format('D')}
+                  </Text>
+                </View>
               </View>
             </View>
             {hasAllDayEventOnDateRange && (
